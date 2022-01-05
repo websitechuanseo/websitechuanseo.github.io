328d16820f23c54512e40155a8dd3d0ceab77f6c
Tran Thanh Long <v.long@b-create.jp>
2022-01-05 13:48:55 +0700

    Long update

--- themes/bet_template/js/main.js
+++ themes/bet_template/js/main.js
@@ -1,19 +1,26 @@
 (function($){
+
+    /* jsMenuSP
+    --------------------------------------------------*/
+    var jsMenuSP = function(){
+        $('.btn-menu').click(function(){
+            $(this).toggleClass('active');
+            $(this).find('.icon').toggleClass('on');
+            $('.h-navi').toggleClass('on');
+        });
+    }
     
     /* jsBacktop
     --------------------------------------------------*/
     var jsBacktop = function(){
         var back_top = $('.backtop'), offset = 200;
-
         back_top.click(function(){
             $("html, body").animate({ scrollTop: 0 }, 800 );
             return false;
         });
-
         if($(window).scrollTop() > offset){ 
             back_top.fadeIn(200);
         }
-
         $(window).scroll(function() {
             if($(window).scrollTop() > offset ) back_top.fadeIn(200); 
             else back_top.fadeOut(200);
@@ -21,6 +28,7 @@
     }
 
     $(document).ready(function() {
+        jsMenuSP();
         jsBacktop();
     });
     
