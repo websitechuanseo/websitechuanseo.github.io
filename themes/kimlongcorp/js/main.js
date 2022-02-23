cefe351761e062150ee52bf5c0af9d5fa091cfb4
Tran Thanh Long <v.long@b-create.jp>
2022-02-23 22:26:07 +0700

    Long up

--- themes/kimlongcorp/js/main.js
+++ themes/kimlongcorp/js/main.js
@@ -31,9 +31,68 @@
         });
     }
 
+
+    /* jsanchor
+    --------------------------------------------------*/
+    $(window).scroll(function () {
+		var banner = $(".page-title").innerHeight();
+		var sbHeight = window.pageYOffset;
+		$(".header.is-blue").addClass("fixed");
+		if ($(window).scrollTop() === 0) {
+			$(".header").removeClass("fixed");
+		}
+		if (sbHeight >= banner - 25) {
+			$(".nav-anchor").addClass("fixed");
+		} else {
+			$(".nav-anchor").removeClass("fixed");
+		}
+	});
+
+     /* jsspace
+    --------------------------------------------------*/
+    $(window).on('resize load', function() {
+        var $this = $(this);
+        var $winW = $this.width();
+        var $containerW = $('.inner01').width();
+        var $margin = ( $winW - $containerW ) / 2 ;
+        $('.space-left').css({
+            'padding-left': $margin
+        });
+        $('.space-right').css({
+            'padding-right': $margin
+        });
+    });
+
+
     $(document).ready(function() {
         jsBacktop();
         jsTabs();
+
+        
+     /* jsslider
+    --------------------------------------------------*/
+    $(".tc-slider").slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: false,
+		// autoplay: true,
+        arrows: false,
+		infinite: true,
+		autoplaySpeed: 2000,
+		speed: 1000,
+		pauseOnHover: false,
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	});
+
+
     });
     
 })(jQuery);
--- themes/kimlongcorp/js/top.js
+++ themes/kimlongcorp/js/top.js
@@ -3,58 +3,26 @@
     /* jsMainVisual
     --------------------------------------------------*/
     var jsMainVisual = function(){
-        $('.mv-item').eq(0).addClass('on');
-        
-        if (document.addEventListener) {
-            document.addEventListener("mousewheel", MouseWheelHandler, false); //IE9, Chrome, Safari, Oper
-            document.addEventListener("DOMMouseScroll", MouseWheelHandler, false); //Firefox
-        } else {
-            document.attachEvent("onmousewheel", MouseWheelHandler); //IE 6/7/8
-        }
-
-        var i = 1;
-        var mouseWheel = true;
-        function MouseWheelHandler(e) {
-            if (!mouseWheel) {
-                return false;
-            }
-            mouseWheel = false;
-            setTimeout(function() {
-                mouseWheel = true;
-            }, 1000);
-            e = window.event || e;
-            var delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
-            var h = window.innerHeight;
-            var section = $('.mv-item');
-            console.log(i);
-            if (i <= section.length && i >= 0) {
-                //scrolling down?
-                if (delta < 0) {
-                    window.scrollTo({
-                        top: h * i,
-                        behavior: "smooth"
-                    });
-                    section.removeClass('on');
-                    section.eq(i).addClass('on');
-                    if(i < section.length) i++;
-                } else {
-                    //scrolling up?
-                    window.scrollTo({
-                        top: h * i,
-                        behavior: "smooth"
-                    });
-                    section.removeClass('on');
-                    section.eq(i).addClass('on');
-                    if(i>0) i--;                   
-                }
-            }
-        }
+        $('.mv-item').eq(0).addClass('active');
+        $('.mainVisual').fullpage({
+            afterLoad: function(anchorLink, index){
+                $('.mv-nav .item').removeClass('active');
+                $('.mv-nav .item').eq(index-1).addClass('active');
+            },
+        });
+        $('.mv-btn-next').click(function(){
+            $.fn.fullpage.moveSectionDown();
+        });
     }
 
     /* jsMvSlider
     --------------------------------------------------*/
     var jsMvSlider = function(){
-        $('.mv-slder').slick();
+        $('.mv-slider').slick({
+            dots: true,
+            prevArrow: '<span class="prev-arrow"><svg class="page-icon"><use xlink:href="#icon-slider-arrow"></use></svg></span>',
+            nextArrow: '<span class="next-arrow"><svg class="page-icon"><use xlink:href="#icon-slider-arrow"></use></svg></span>',
+        });
     }
 
     $(document).ready(function() {
