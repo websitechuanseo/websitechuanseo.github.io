0b5d095b56339878719a63766563f3895c0291ee
Tran Thanh Long <v.long@b-create.jp>
2022-11-30 11:24:01 +0700

    Long update

--- js/main.js
+++ js/main.js
@@ -3,40 +3,23 @@
     $(window).on('load', function(){
         $('#preloader').fadeOut(1000);
     });
-    
-    /* jsHeader
-    --------------------------------------------------*/
-    var jsHeader = function(){
-        var lastScrollTop = 0;
-        $(window).on('scroll',function(){
-        var st = $(this).scrollTop();
-        if (st > lastScrollTop){
-            $('#header').addClass('header-hide');
-        } else {
-            $('#header').removeClass('header-hide');
+
+    $(document).ready(function(){
+        if($(".js-main-slider").length) {
+            $(".js-main-slider").slick({
+                autoplay: true,
+                autoplaySpeed: 3000,
+                speed: 600,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: false,
+                fade: true,
+                pauseOnHover: false,
+                pauseOnFocus: false,
+                cssEase: 'linear'
+            });
         }
-        lastScrollTop = st;
-        });        
-    }
-
-    /* jsSlider
-    --------------------------------------------------*/
-    var jsSlider = function(){
-        $('.mainSlider').slick({
-            dots: false,
-            infinite: true,
-            autoplay: 2000,
-            speed: 1500,
-            slidesToShow: 1,
-            pauseOnHover: false,
-            fade: true,
-            arrows: false,
-        });
-    }
-
-    $(document).ready(function() {
-        // jsHeader();
-        // jsSlider();
     });
     
 })(jQuery);
